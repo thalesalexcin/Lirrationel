274c09643298b0cfb2e44829cda2438d0a10a7c0
thalesalexcin@gmail.com <thalesalexcin@gmail.com>
2016-01-04 21:20:14 +0100

    - Backgroud resizing

--- Lirrationel/Assets/Scripts/Elements/Decoratives/WaterHell.cs
+++ Lirrationel/Assets/Scripts/Elements/Decoratives/WaterHell.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.Scripts.Elements
 {
     public class WaterHell : DecorativeElement
     {
+
+        void Start()
+        {
+            Resize();
+        }
+
+        void Resize()
+        {
+            SpriteRenderer sr = GetComponent<SpriteRenderer>();
+            if (sr == null) return;
+
+            transform.localScale = new Vector3(1, 1, 1);
+
+            float width = sr.sprite.bounds.size.x;
+            float height = sr.sprite.bounds.size.y;
+
+
+            float worldScreenHeight = Camera.main.orthographicSize * 2f;
+            float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+            Vector3 xWidth = transform.localScale;
+            xWidth.x = worldScreenWidth / width;
+            transform.localScale = xWidth;
+            //transform.localScale.x = worldScreenWidth / width;
+            Vector3 yHeight = transform.localScale;
+            yHeight.y = worldScreenHeight / height;
+            transform.localScale = yHeight;
+            //transform.localScale.y = worldScreenHeight / height;
+
+        }
     }
 }
